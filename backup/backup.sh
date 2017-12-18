39b87eacc340a97324cfc8e64db8b856766b8477
Troels Agergaard Jacobsen <troels@ajonline.dk>
2017-12-18 21:36:47 +0100

    Add python3 script version of enote

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/python2.7 /app/enote/enote/enote.py --path /data/Evernote --config /config/enote download
+/usr/bin/python3 /app/enote.py --path /data/Evernote --config /config/enote
 
 yes | /usr/bin/vdirsyncer --config /config/vdirsyncer/config --verbosity ERROR discover
 /usr/bin/vdirsyncer --config /config/vdirsyncer/config sync
